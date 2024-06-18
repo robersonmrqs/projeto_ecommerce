01c63d1a04bee949d8648907b65443e8155e63e8
unknown <robersonmrqs@gmail.com>
2024-06-18 11:56:47 -0300

    Configuração inicial do carrinho

--- loja/views.py
+++ loja/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 def homepage(request):
@@ -29,9 +29,19 @@ def ver_produto(request, id_produto, id_cor=None):
         if id_cor:
             itens_estoque = ItemEstoque.objects.filter(produto=produto, quantidade__gt=0, cor__id=id_cor)
             tamanhos = {item.tamanho for item in itens_estoque}
-    context = {'produto': produto, 'itens_estoque': itens_estoque, 'tem_estoque': tem_estoque, 'cores': cores, 'tamanhos': tamanhos,
-               'nome_cor_selecionada': nome_cor_selecionada}
+    context = {'produto': produto, 'tem_estoque': tem_estoque, 'cores': cores, 'tamanhos': tamanhos, 'nome_cor_selecionada': nome_cor_selecionada}
     return render(request, 'ver_produto.html', context)
+
+def adicionar_carrinho(request, id_produto):
+    if request.method == 'POST' and id_produto:
+        dados = request.POST.dict()
+        tamanho = dados.get('tamanho')
+        id_cor = dados.get('cor')
+        if not tamanho:
+            return redirect ('loja')
+        return redirect('carrinho')
+    else:
+        return redirect('loja')
 
 def carrinho(request):
     return render(request, 'carrinho.html')
